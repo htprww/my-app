a512ed764a9efa189589fd28aeb829b0e497338e
htprww <htprww@gmail.com>
2018-02-24 18:56:33 -0500

    a

--- src/index.js
+++ src/index.js
@@ -2,80 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: null,
-    };
+    this.state = {str: 'abc'};
   }
 
   render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: this.state.value + 1})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
-    };
-  }
-
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.str}.</h2>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
+const ele = <Clock />;
 
 ReactDOM.render(
-  <Game />,
+  ele,
   document.getElementById('root')
 );
+
+window.x = ele;
